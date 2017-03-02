b7035423a4647680fb60439d369325d181291c0f
Mister Tandon <enggparveshtandon@gmail.com>
2017-03-02 23:38:21 +0530

    Updated chat code, added private messaging feature.

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var HTTP = require('http');
 var EXPRESS = require('express');
 var path = require('path');
@@ -6,6 +8,7 @@ var app = EXPRESS();
 var httpServer = HTTP.createServer(app);
 var SOCKET_IO = require('socket.io').listen(httpServer);
 var userNamesObj = {};
+var socketsRefObj = {};
 
 
 httpServer.listen(3070);
@@ -24,24 +27,90 @@ app.get('/', function (request, response) {
 
 SOCKET_IO.sockets.on('connection', function (socket) {
 
-	socket.on('send_message', function (data) {
+	socket.on('send_message', function (data, callBack) {
 
-		SOCKET_IO.sockets.emit('new_message', {
+		var message;
+		var privateMessageRecog;
+		var isPrivateMessaging;
+		var messageObjForEvent = {};
 
-			"userName": socket.username,
-			"message": data
-		});
+
+		message = data.trim();
+
+		/**
+		 * Validating, if accidently empty string inserted by user.
+		 */
+		if (_.isEmpty(message)) {
+
+			callBack(false, "Entered message was empty.");
+
+			return;
+		}
+
+		isPrivateMessaging = false;
+
+		/**
+		 * `private messaging` will start with `@`, here we are recognizing if
+		 * 	user send a personal message.
+		 */
+		if (message.charAt() === '@') {
+
+			var firstWhiteSpaceIndex;
+			var targetUserName;
+			var particularMessage;
+
+			firstWhiteSpaceIndex = message.indexOf(' ');
+			targetUserName = message.substr(1, firstWhiteSpaceIndex - 1);
+			particularMessage = message.substr(firstWhiteSpaceIndex + 1);
+
+			if (targetUserName in userNamesObj) {
+
+				isPrivateMessaging = true;
+
+				messageObjForEvent['targetUserName'] = targetUserName;
+				messageObjForEvent['message'] = particularMessage;
+
+				callBack(true, "Message has been sent privately to " + targetUserName);
+			} else {
+
+				callBack(false, "Targeted user i.e. " + targetUserName + " not found into live users list.");
+
+				return;
+			}
+		}
+
+		if (isPrivateMessaging) {
+
+			socketsRefObj[targetUserName].emit('private_messaging', messageObjForEvent);
+
+			return;
+		}
+
+		messageObjForEvent = null;
+
+		if (!isPrivateMessaging) {
+
+			SOCKET_IO.sockets.emit('new_message', {
+
+				"userName": socket.username,
+				"message": message
+			});
+
+			return;
+		}
+
 	});
 
 	socket.on('is_valid_username', function (data, callBack) {
 
-		if (data in userNamesObj) {
+		if (!_.isEmpty(userNamesObj) && data in userNamesObj) {
 
 			callBack(false);
 		} else {
 
-			socket.username = data;
-			userNamesObj.data = 1;
+			socket.username = data.trim();
+			userNamesObj[data] = 1;
+			socketsRefObj[data] = socket;
 
 			callBack(true);
 
